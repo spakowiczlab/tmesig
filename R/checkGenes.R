8777cfb8a08e1c11dd7a5aa356903be5ff5f6989
hoyd44 <rhoyd44@gmail.com>
2021-12-01 20:37:05 +0000

    Add function for quick gene set check

---
+++ R/checkGenes.R
@@ -0,0 +1,20 @@
+#' Check presence of required genes
+#'
+#' Genes may appear to be missing from the data set due to changing naming conventions. This function returns gene names that are required for the specified calculation
+#' so that they can be addressed, possibly by adding updated gene names to the vector before passing to the score calculator.
+#' @param genevec A character vector containing the genes available in the data set the calculation is to be performed on.
+#' @param score The name of the score whose dependent genes should be checked.
+#' @return A character vector containing any genes required by the score and missing in the data set.
+#' @export
+
+
+checkGenes <- function(genevec, score){
+
+  expected.genes <- inputGenes(score)
+  missing.genes <- setdiff(expected.genes, genevec)
+
+  all.present <- ifelse(length(missing.genes) >= 1, F, T)
+  print(all.present)
+
+  return(outval)
+}
