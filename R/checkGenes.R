87051859f97eb646700dc1af916465a9cf665256
hoyd44 <rhoyd44@gmail.com>
2022-06-23 15:57:35 +0100

    Allow checkGenes to accept user defined expected gene vector

--- R/checkGenes.R
+++ R/checkGenes.R
@@ -4,13 +4,17 @@
 #' so that they can be addressed, possibly by adding updated gene names to the vector before passing to the score calculator.
 #' @param genevec A character vector containing the genes available in the data set the calculation is to be performed on.
 #' @param score The name of the score whose dependent genes should be checked.
+#' @param expected.genes A vector of the genes expected in the data. Can be provided instead of score.
 #' @return A character vector containing any genes required by the score and missing in the data set.
 #' @export
 
 
-checkGenes <- function(genevec, score){
+checkGenes <- function(genevec, score = NULL, expected.genes = NULL){
 
-  expected.genes <- inputGenes(score)
+  if(!is.null(score)){
+    expected.genes <- inputGenes(score)
+  }
+
   missing.genes <- setdiff(expected.genes, genevec)
 
   all.present <- ifelse(length(missing.genes) >= 1, F, T)
